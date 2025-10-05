2e9017314cdebc298be6319e96842c26d28cecd0
wyofalcon <wyofalcon@outlook.com>
2025-10-05 16:57:48 -0500

    fix: add .js extension to reportWebVitals import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
